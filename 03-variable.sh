7c860472d2799f9cf5b6f9ba9bb8a15fac3f32da
vinaydms <vinay.dms16@gmail.com>
2025-09-22 21:38:41 +0530

    variable

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=lalitha # there is no space between variable and value
+person2=bala
+
+echo "$person1:: Hello $person2, how are you?"
+echo "$person2:: Hi $person1, I am fine. How are you?"
+echo "$person1:: I am fine too. how is your work?"
+echo "$person2:: not bad. I am thinking to upgrade to Devops"
+
