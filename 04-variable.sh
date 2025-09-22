ecef56beb98f186812b0655f9f1b3b005a0028d6
vinaydms <vinay.dms16@gmail.com>
2025-09-22 21:53:56 +0530

    variable

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #declaring the variable
-person1=waste # there is no space between variable and value
-person2=idiot
+# argument
+person1=1 # there is no space between variable and value
+person2=2
 # referring the variable
 echo "$person1:: Hello $person2, how are you?"
 echo "$person2:: Hi $person1, I am fine. How are you?"
 echo "$person1:: I am fine too. how is your work?"
 echo "$person2:: not bad. I am thinking to upgrade to Devops"
-
