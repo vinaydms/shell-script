9f220e82e9f917a90aa75264fd7b534ac17d50a9
vinaydms <vinay.dms16@gmail.com>
2025-09-22 21:56:07 +0530

    variable

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,8 +2,8 @@
 
 #declaring the variable
 # argument
-person1=1 # there is no space between variable and value
-person2=2
+person1=$1 # there is no space between variable and value
+person2=$2
 # referring the variable
 echo "$person1:: Hello $person2, how are you?"
 echo "$person2:: Hi $person1, I am fine. How are you?"
