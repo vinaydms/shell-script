e87d76e404c38081a3f6496bea9880fac9eb4442
vinaydms <vinay.dms16@gmail.com>
2025-09-22 22:23:51 +0530

    array

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MOVIES=("RRR" "DJTILLU" "LEADER")
+#size of above list-3
+# how indexs are there in above - 0,1,2
+echo "first movie is: ${MOVIES[0]}"
+echo "second movie is: ${MOVIES[1]}"
+echo "third movie is: ${MOVIES[2]}"
+echo "all movies is: ${MOVIES[@]}"
