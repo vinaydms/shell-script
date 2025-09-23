cdc2b0185e3a662df114772b7d329851844383df
vinaydms <vinay.dms16@gmail.com>
2025-09-23 17:33:08 +0530

    host

--- 08-special-vari.sh
+++ 08-special-vari.sh
@@ -5,4 +5,5 @@ echo "script name: $0"
 echo "current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
+echo "host name: $HOSTNAME"
 
