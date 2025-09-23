49c052115ab242898f31a94b9b1431f48a219882
vinaydms <vinay.dms16@gmail.com>
2025-09-23 18:12:06 +0530

    push

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 NUMBER=$!
+
 if [ $NUMBER -gt 10 ]
 then
     echo "Given number $NUMBER is greater than 10"
 else
     echo "Given number $NUMBER is less than 10"
 fi
+#-gt, -lt, -eq, -ge, -le
