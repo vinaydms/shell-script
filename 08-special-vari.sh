0922987f41dcb46fecb8fe2a452baa63dd9b92ad
vinaydms <vinay.dms16@gmail.com>
2025-09-23 17:30:52 +0530

    script

--- 08-special-vari.sh
+++ 08-special-vari.sh
@@ -4,4 +4,5 @@ echo "Number of variables passed: $#"
 echo "script name: $0"
 echo "current working directory: $PWD"
 echo "Home directory of current user: $HOME"
+echo "which user is running this script: $USER"
 
