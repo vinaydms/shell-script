d01d611b0c69e5e682163aef4f0a767f981190ab
vinaydms <vinay.dms16@gmail.com>
2025-09-27 17:10:47 +0530

    function

---
+++ 11-function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USERID=$(id -u)
+
+VALIDATE(){ 
+    echo "exit status: $1"
+    echo "what are you doing: $2"
+
+}
+if [USERID -ne 0]
+then
+    echo "please run this script with root acces"
+else
+    echo "you are a super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing MYSQL"
+
+dnf install git -y
+VALIDATE $? "Installing Git"
