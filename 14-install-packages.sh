456d579b8798ceab7576f6d541d6214f700ca682
vinaydms <vinay.dms16@gmail.com>
2025-09-28 18:40:53 +0530

    install

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
@@ -12,4 +16,10 @@ fi
 for i in $@
 do
     echo "packages to install: $i"
+    dnf list installed $i
+    if [ $? eq 0]
+    then
+        echo "$i already installed...SKIPPING"
+    else
+        echo "$i not installed...Need to install"
 done
