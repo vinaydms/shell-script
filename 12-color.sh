d1efebe11e6044cd186244b585ac2de964d49fe3
vinaydms <vinay.dms16@gmail.com>
2025-09-28 16:30:59 +0530

    color

--- 12-color.sh
+++ 12-color.sh
@@ -15,10 +15,10 @@ echo "script started executing at: $TIMESTAMP"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-         echo -e "$2....$R FAILURE  "
+         echo -e "$2....$R FAILURE $N "
          exit 1
     else
-        echo -e "$2.....$G SUCCESS  "
+        echo -e "$2.....$G SUCCESS $N "
     fi
 }
 
