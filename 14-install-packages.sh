4c104168eb8ac5787d406378f4d78075b6592e89
vinaydms <vinay.dms16@gmail.com>
2025-09-28 18:55:13 +0530

    install

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -5,6 +5,11 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
@@ -19,7 +24,7 @@ do
     dnf list installed $i
     if [ $? -eq 0 ]
     then
-        echo "$i already installed...SKIPPING"
+        echo -e "$i already installed...$Y SKIPPING $N"
     else
         echo "$i not installed...Need to install"
     fi
