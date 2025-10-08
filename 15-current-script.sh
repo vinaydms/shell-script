afa15eb97da68367dbd9454099612d1f032f1b67
vinaydms <vinay.dms16@gmail.com>
2025-10-08 17:12:59 +0530

    shell

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -4,6 +4,6 @@ echo "Before calling other script, course:$COURSE"
 
 echo "Process id of current shell script: $$"
 
-./16-other-script.sh
-
+#./16-other-script.sh
+source ./16-other-script.sh
 echo "After calling other script, course:$COURSE"
