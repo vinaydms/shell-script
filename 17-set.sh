fa0335b48158d062749746202a295774839286c3
vinaydms <vinay.dms16@gmail.com>
2025-10-09 16:16:58 +0530

    push

---
+++ 17-set.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root access"
+    exit 1 # Manually exit if error comes.
+else 
+    echo "you are super user"
+fi
+
+dnf install mysqllll -y
+
+dnf install git -y 
+
+echo "is script proceeding?"
+
