642b6961f2694a81d6aa00e2abffb7d78d34cf12
vinaydms <vinay.dms16@gmail.com>
2026-05-23 16:01:20 +0530

    shell

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
+
 echo "hello, we start shall-script"
 echo "i am doing work on project basis"
---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : Hello suresh, How are you."
+echo "suresh : Hi ramesh, I am fine, how are you."
+echo "Ramesh : I am fine too, how is your work ? "
+echo "Suresh : not bad, I am thinking to upgrade to devops"
