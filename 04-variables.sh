76e706266320c1b22e90bdb0f2fe1dbf14169b15
vinaydms <vinay.dms16@gmail.com>
2026-05-29 09:25:21 +0530

    shell

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#declaring of variable
-PERSON1=vinay # no space between = and value
-PERSON2=mallesh
+PERSON1=$1
+PERSON2=$2
 
 #referring variable
 echo "$PERSON1 : Hello $PERSON2, How are you."
 echo "$PERSON2 : Hi , I am fine, how are you."
 echo "$PERSON1 : I am fine too, how is your work ? "
 echo "$PERSON2 : not bad, I am thinking to upgrade to devops"
-
