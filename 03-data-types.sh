62ab5a6ec1f996d38bbaaca594f52e7e3255eaee
vinaydms <vinay.dms16@gmail.com>
2026-05-29 08:11:25 +0530

    shell

--- 03-data-types.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "hello i am working in amazon, what about you prasad"
-
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=RAMESH # no space between = and value
+PERSON2=SURESH
+
+echo "$PERSON1 : Hello $PERSON2, How are you."
+echo "$PERSON2 : Hi , I am fine, how are you."
+echo "$PERSON1 : I am fine too, how is your work ? "
+echo "$PERSON2 : not bad, I am thinking to upgrade to devops"
+
