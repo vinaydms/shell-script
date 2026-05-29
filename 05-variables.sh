0f8521df62fb414945e1ef0303a1d6e741be91d7
vinaydms <vinay.dms16@gmail.com>
2026-05-29 09:47:11 +0530

    shell

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,10 @@
 
 echo "please enter USERNAME::"
 
-read USERNAME #here USERNAME is variable
+read -s USERNAME #here USERNAME is variable
 
 echo "Please enter PASSWORD::"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "Username is: $USERNAME, password is: $PASSWORD"
