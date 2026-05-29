d6cb8d909293075096b53d097d3ef0b706130744
vinaydms <vinay.dms16@gmail.com>
2026-05-29 11:07:07 +0530

    shell

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTillu" "Murari")
+
+# size of above array is 3.
+#index are 0,1,2
+
+#list always starts with 0.
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[@]}"
+#echo "First Movie is: ${MOVIES[@]}"
