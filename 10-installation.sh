7afd3ef0c422e1100202e6b3b651d51b667c4cf0
vinaydms <vinay.dms16@gmail.com>
2026-05-29 15:51:30 +0530

    shell

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then    
     echo "Please run this script with root access."
+    exit 1 # manually exit other than zero 
 else
     echo "you are super user."
 fi
