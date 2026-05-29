e490b4aa3a780005f5f2a9272ec99c68906a484d
vinaydms <vinay.dms16@gmail.com>
2026-05-29 10:58:39 +0530

    shell

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,7 +6,7 @@ PERSON2=mallesh
 
 #referring variable
 echo "$PERSON1 : Hello $PERSON2, How are you."
-echo "$PERSON2 : Hi , I am fine, how are you."
+echo "${PERSON2} : Hi , I am fine, how are you."
 echo "$PERSON1 : I am fine too, how is your work ? "
 echo "$PERSON2 : not bad, I am thinking to upgrade to devops"
 
