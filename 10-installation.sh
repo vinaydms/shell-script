f3f2b31910cf3f67329c68e88947810d50e4a5b6
vinaydms <vinay.dms16@gmail.com>
2026-05-29 15:59:35 +0530

    shell

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,6 +10,14 @@ else
     echo "you are super user."
 fi
 
-dnf install mysql -y
+dnf install mysqlll -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql... FAILURE"
+    exit 1
+fi
+
+dnf install git -y
 
 echo "is script proceeding?"
