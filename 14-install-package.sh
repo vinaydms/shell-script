496e9be82ee884bfbf19fe7bc07d29c7116f5d2a
vinaydms <vinay.dms16@gmail.com>
2026-06-03 12:44:46 +0530

    shell

--- 14-install-package.sh
+++ 14-install-package.sh
@@ -4,18 +4,18 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-#if [ $USERID -ne 0 ]
-#then    
-    #echo "Please run this script with root access."
-    #exit 1 # manually exit other than zero 
-#else
-    #echo "you are super user."
-#fi
-
-if [ $USERID -eq 0 ]
-then
+if [ $USERID -ne 0 ]
+then    
+    echo "Please run this script with root access."
+    exit 1 # manually exit other than zero 
+else
     echo "you are super user."
 fi
+
+# if [ $USERID -eq 0 ]
+# then
+#     echo "you are super user."
+# fi
 
 for i in $@
 do
