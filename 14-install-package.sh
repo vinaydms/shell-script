409877dc67c8bf245376e564cb3c1b0f1170f77e
vinaydms <vinay.dms16@gmail.com>
2026-06-03 09:26:40 +0530

    shell

---
+++ 14-install-package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+USERID=$(id -u)
+
+#if [ $USERID -ne 0 ]
+#then    
+    #echo "Please run this script with root access."
+    #exit 1 # manually exit other than zero 
+#else
+    #echo "you are super user."
+#fi
+
+if [ USERID eq 0 ]
+then
+    echo "please run this script root access."
+    exit 0
+fi
+
+echo "All pacakages: $@"
