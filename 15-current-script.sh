054a0f2e36cbaea1140639322e9291dc9510cbaf
vinaydms <vinay.dms16@gmail.com>
2026-06-06 08:26:26 +0530

    mysql

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,8 @@ COURSE="Devops from current script"
 echo "Before calling other script,course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-script.sh
+#./16-other-script.sh
+
+source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
