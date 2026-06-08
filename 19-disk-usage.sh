7f92af1efbdfc47f382faeaccb4db17620703461
vinaydms <vinay.dms16@gmail.com>
2026-06-08 13:46:34 +0530

    shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -10,9 +10,9 @@ USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
 FOLDER=$(echo $line | awk -F " " '{print $NF}')
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then
-    MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE"
+    MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
 fi
 
 done <<< $DISK_USAGE
 
-echo "Message: $MESSAGE"
+echo -e "Message: $MESSAGE"
